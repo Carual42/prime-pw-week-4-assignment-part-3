0520b4f1b93b539b779f1ff862a13c54dbc7e097
Alex Carufel <Carual42@gmail.com>
2022-06-19 17:49:46 -0500

    addItem function updated

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,16 +3,36 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [] // Basket array
 
+// global variable for max items
+const maxItems = 5;
+
+// function to test if basket is full
+function isFull(array, max){
+    if(array.length < max){
+        return false;
+    } else if (array.length >= max){
+        return true;
+    }
+}
+console.log(isFull(basket, maxItems))
+
 // function to add items
 function addItem (item){
+        if (isFull(basket, maxItems) === false){
     basket.push(item);
     console.log(item);
-    return item;
+    return true
+        } else if (isFull(basket, maxItems) === true){
+    console.log('no room for more items')
+    return false;
+    }   
 }
+
 // calls addItem function
 addItem('Apple');
 addItem('Peanuts');
 addItem('Candy Bar')
+
 // logs and test the item was added
 console.log('add apples', basket)
 
@@ -22,14 +42,18 @@ function listItems(array){
         console.log(array[i]);
     }
 }
+
 //calls listItem function and test it
 listItems(basket);
 console.log(basket)
 // function to empty basket
 function empty(array){
-    array.length = 0
+    array.length = 0;
 }
+
 // calls function and test it
 empty(basket);
-console.log('basket is now', basket)
+console.log('basket is now', basket);
 
+//logs and test isFull function
+console.log(isFull(basket, maxItems))
