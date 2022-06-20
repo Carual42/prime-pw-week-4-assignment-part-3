6cb81acafaf31df22ddb34f3bb403fd3000a435e
Alex Carufel <Carual42@gmail.com>
2022-06-19 20:15:58 -0500

    finished

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -29,6 +29,12 @@ function addItem (item){
 }
 
 // function to remove items
+function removedItem(item, array){
+    let removed = array.indexOf(item);
+    console.log('lets remove', removed);
+    console.log(array.splice(removed, 1));
+    return;
+}
 
 // calls addItem function
 addItem('Apple');
@@ -36,12 +42,15 @@ addItem('Peanuts');
 addItem('Candy Bar')
 
 // logs and test the item was added
-console.log('add apples', basket)
+console.log('add apples, peanuts, and candy bar', basket)
+
+// calls removedItem function
+removedItem('Peanuts', basket)
 
 // function to list items in array
 function listItems(array){
     for(let i = 0; i<array.length; i++){
-        console.log(array[i]);
+        console.log('Check', array[i]);
     }
 }
 
